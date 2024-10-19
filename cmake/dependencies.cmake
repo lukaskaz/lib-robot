8c41f0d9851c0460c4ed5449a1df3ec8d41f5983
lukaskaz <lukask84@o2.pl>
2024-10-19 23:58:24 +0200

    Responsibility separation, third stage (#4)
    Signed-off-by: Lukasz Kazmierczak <lukask84@o2.pl>

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,7 +1,6 @@
 cmake_minimum_required(VERSION 3.10)
 
 find_package(Boost COMPONENTS program_options REQUIRED)
-include_directories(${Boost_INCLUDE_DIRS})
 include(ExternalProject)
 
 include_directories(${source_dir}/inc)
@@ -50,28 +49,27 @@ EXTERNALPROJECT_ADD(
 include_directories(${source_dir}/include)
 link_directories(${build_dir}/build)
 
-set(source_dir "${CMAKE_BINARY_DIR}/libcpr-src")
-set(build_dir "${CMAKE_BINARY_DIR}/libcpr-build")
+set(source_dir "${CMAKE_BINARY_DIR}/libhttp-src")
+set(build_dir "${CMAKE_BINARY_DIR}/libhttp-build")
 
 EXTERNALPROJECT_ADD(
-  libcpr
-  GIT_REPOSITORY    https://github.com/libcpr/cpr.git
-  GIT_TAG           1.10.5
-  PATCH_COMMAND     ${patching_cmd}
-  PREFIX            libcpr-workspace
+  libhttp
+  GIT_REPOSITORY    https://github.com/lukaskaz/lib-http.git
+  GIT_TAG           main
+  PATCH_COMMAND     ""
+  PREFIX            libhttp-workspace
   SOURCE_DIR        ${source_dir}
   BINARY_DIR        ${build_dir}
   CONFIGURE_COMMAND mkdir /${build_dir}/build &> /dev/null
-  BUILD_COMMAND     cd ${build_dir}/build && cmake -D CPR_USE_SYSTEM_CURL=ON
+  BUILD_COMMAND     cd ${build_dir}/build && cmake -D BUILD_SHARED_LIBS=ON
                     ${source_dir} && make
   UPDATE_COMMAND    ""
   INSTALL_COMMAND   ""
   TEST_COMMAND      ""
 )
 
-include_directories(${source_dir}/include)
-include_directories(${build_dir}/build/cpr_generated_includes)
-link_directories(${build_dir}/build/lib)
+include_directories(${source_dir}/inc)
+link_directories(${build_dir}/build)
 
 set(source_dir "${CMAKE_BINARY_DIR}/libtts-src")
 set(build_dir "${CMAKE_BINARY_DIR}/libtts-build")
